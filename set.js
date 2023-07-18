7db398b655c30046c44483daaac363e26da6b6ce
Jay <Jaykalathiya251251@gmail.com>
2023-07-18 15:42:47 +0530

    set method

---
+++ set.js
@@ -0,0 +1,74 @@
+
+
+// New Set()
+
+{
+    // const x= new Set([10,20,30]);
+    // console.log(x.size);
+}
+
+// Add ()
+
+{
+    // const x=new Set();
+
+    // x.add("Jay");
+    // x.add("Ankit");
+    // x.add("Rutvik");
+
+    // console.log(x);
+}
+
+{
+    // const y =new Set();
+
+    // const a=10;
+    // const b=20;
+    // const c=60;
+
+    // y.add(a);
+    // y.add(b);
+    // y.add(c);
+
+    // console.log(y.size);
+}
+
+// Delete()
+
+{
+    // const x=new Set(
+    //     "a",
+    //     "b",
+    //     "c",
+    //     "d"
+    // );
+    // x.delete("b");
+
+    // console.log(x);
+}
+
+// Has
+{
+    // const x=new Set(
+    //     "a",
+    //     "b",
+    //     "c",
+    //     "d"
+    // );
+    // let y=x.has("r");
+    // console.log(y);
+}
+
+// Values()
+
+{
+    // const x=new Set(
+    //     "a",
+    //     "b",
+    //     "c",
+    //     "d"
+    // );
+    // let y=x.values();
+
+    // console.log(y);
+}
