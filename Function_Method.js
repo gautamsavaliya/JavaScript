f7f2d142fdcc598bfb92f5caad128951d345d19d
Jay <Jaykalathiya251251@gmail.com>
2023-07-31 15:53:51 +0530

    Function Method

---
+++ Function_Method.js
@@ -0,0 +1,51 @@
+// Javascript Callback Function ----
+
+
+// function fun(name)
+// {
+//     document.write("Hi"+" "+name)
+// };
+// fun("Suresh");
+
+{
+    // function fun(name,Callback)
+    // {
+    //    console.log("Hi"+" "+name);
+    //     Callback();
+    // };
+
+    // function callme()
+    // {
+    //     console.log("CallBacke function");
+    // };
+
+    // fun("Suresh",callme)
+}
+
+/* JavaScript Arrow Function */
+
+{
+    // function expression
+    // let x=fun(a,b)
+    // {
+    //     return a*b;
+    // }
+    
+    // Using Arrow Function
+
+    // let a=(x,y)=>x*y;
+}
+
+// Arrow Function with No Argument
+
+{
+    // let fun=()=>console.log("Hello");
+    // fun();
+}
+
+// Arrow Function With One Argument
+
+{
+    // let fun=x=>console.log(x);
+    // fun("Hi");
+}
