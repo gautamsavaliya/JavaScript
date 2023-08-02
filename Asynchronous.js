3617390a4799142a681f8a08eb694db11e4b43b0
Jay <Jaykalathiya251251@gmail.com>
2023-08-02 15:56:27 +0530

    New

---
+++ Asynchronous.js
@@ -0,0 +1,30 @@
+// Javascript setTimeout()
+
+{
+    // settimeout(function,milliseconds)
+}
+
+// setTimeout
+
+{
+    // function fun()
+    // {
+    //     console.log("Hello World");
+    // }
+
+    // setTimeout(fun,3000);
+    // console.log(fun);
+}
+
+// setTimeout() with Interval id
+
+{
+    // function fun()
+    // {
+    //     console.log("Hello World");
+    // }
+
+    // let IntervalId=setTimeout(fun,3000);
+    // let IntervalIds=setTimeout(fun,3000);
+    // console.log("Id :" +IntervalId);
+}
---
+++ Class.js
@@ -0,0 +1,62 @@
+// JavaScript Classes
+
+{
+    // function person()
+    // {
+    //     this.name="Ramesh",
+    //     this.age=18
+    // }
+
+    // const person1= new person();
+
+    // console.log(person1);
+
+}
+
+// Crating A Class
+{
+    // class person{
+    //     constructor(name)
+    //     {
+    //         this.name=name;
+    //     }
+    // }
+}
+
+{
+    // class person{
+    //     constructor(name)
+    //     {
+    //         this.name=name;
+    //     }
+    // }
+
+    // const a=new person("Ramesh");
+    // const b=new person("Suresh");
+
+    // console.log(a);
+    // console.log(a.name);
+
+    // console.log(b);
+    // console.log(b.name);
+}
+
+{
+    // class person{
+    //     constructor(name)
+    //     {
+    //         this.name=name;
+    //     }
+    // }
+
+    // function abc()
+    // {
+    //     console.log(`Hello How Are You Mr. ${this.name}`);
+    // }
+
+    // let person1=new person('Hardik');
+
+    // console.log(person1.name);
+
+    // person1.abc();
+}
